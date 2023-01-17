5b5cbf95f87b3fced468cab9297fb5d475c1176b
Evgeniy Bratenkov <giertrut@yandex.ru>
2023-01-17 21:11:09 +0300

    task04

---
+++ Task_04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4 * : Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+
+int num = new Random().Next(100, 999);
+
+
+int c = num / 10 % 10;
+int b = num % 10;
+int a = num / 10;
+
+Console.WriteLine(num);
+Console.WriteLine((a - c) + b);
+
+
+
